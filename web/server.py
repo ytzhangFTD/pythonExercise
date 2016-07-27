3b0c1ae6e20c600731cf4858329dc5ca1181a057
wb-zhangyitong <wb-zhangyitong@alibaba-inc.com>
2016-07-27 10:24:05 +0800

    update

--- web/server.py
+++ web/server.py
@@ -3,7 +3,7 @@
 # 从wsgiref模块导入:
 from wsgiref.simple_server import make_server
 # 导入我们自己编写的application函数:
-from hello import application
+from webApplication import application
 
 # 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
 httpd = make_server('', 8000, application)
